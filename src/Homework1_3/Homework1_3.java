c91b5ee96b4ac97043738837744d4db287c099f8
GyuHo123 <74482513+GyuHo123@users.noreply.github.com>
2022-03-25 20:58:36 +0900

    written(Homework1_3.java) : code written

--- src/Homework1_3/Homework1_3.java
+++ src/Homework1_3/Homework1_3.java
@@ -1,4 +1,15 @@
 package Homework1_3;
 
 public class Homework1_3 {
+    public static void main(String[] args)
+    {
+        int nums[] = {1, 2, 3, 4, 5, 6};
+        int sum = 0;
+        for(int i = 0; i < nums.length; i++)
+        {
+            sum += nums[i];
+            nums[i] = sum;
+            System.out.println(nums[i]);
+        }
+    }
 }
